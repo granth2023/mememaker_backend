3cfad0d1a5f09a1b78a0e9d6f3d4dd76e91f0683
granth2023 <grantharris38@gmail.com>
2023-06-02 09:51:48 -0400

    allroutes_controllers

--- Routes/firstRoutes.js
+++ Routes/firstRoutes.js
@@ -1,8 +1,46 @@
 import express from 'express';
-import getMemes from '../Controllers/user.controller.js'
+import { getMemes, getMemesById, createMeme, updateMemeByID, deleteMeme, getSubmissions, getSubmissionById, createSubmission, updateSubmission, deleteSubmission} from '../Controllers/user.controller.js'
+// import createMeme from '../Controllers/user.controller.js'
 
 const router = express.Router();
+//need get route for second model submissions-- 
+//get submissions for specific meme, so based off meme reference, /memes/:memeID/submission  
 router.get('/memes', getMemes);
+router.get('/memes/:id', getMemesById);
+router.get('/submissions', getSubmissions);
+router.get('/submissions/:id', getSubmissionById);
 
+
+//need post route for second model 
+router.post('/memes', createMeme);
+router.post('/submissions', createSubmission);
+
+//need put route for second model 
+//submission/:submissionID 
+router.put('/memes/:id', updateMemeByID);
+router.put('/submissions/:id', updateSubmission);
+
+
+router.delete('/memes/:id', deleteMeme);
+router.delete('/submissions/:id', deleteSubmission);
+
+
+//delete submission:    /submission/:submissionID 
+
+//submissions: like comments on a post, memes is the post 
 
 export default router 
+
+
+//post routes/controllers: front end -- create that repo, put in the folder repo lives too, and 
+
+//restful api --config file and services file and then merging --- handle 
+//inside services, services for each backend to talk to front end --- services will have each method from controller---- create meme: inside services, create meme funciton   create meme async and thena  body for bottom text --- await api route . post to /memes,  inside post request send body 
+//create specific route in services for posting text to image "get submissions for memes as a name of a services method "--->this method will take a memeID ---> don't worry about mulitple 
+//add route in backend get Meme by ID 
+
+//user would be a third model 
+
+//FIRST THING: SET UP SERVICES AND API CONFIG --
+// SERVICES: CLIENT FOLDER HTML/CSS/JS AND ANOTHER FOLDER CALLED SERVICES AND SERVICES.JS AND CONTROLLER
+// SERVICES.JS METHODS FOR JAVASCRIPT SERVICES METHODS API 
